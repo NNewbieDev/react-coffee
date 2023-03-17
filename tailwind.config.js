9ab5cd12c15eb0fd208597ee92fea6699c889406
Huy P <NNewbieDev>
2023-03-17 10:12:31 +0700

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    colors: {
+      'yellow': {
+        900: "#876445",
+        600: "#CA965C",
+      },
+      'orange': {
+        200: "#E3CAA5",
+      },
+    },
+  },
+  plugins: [],
+}
