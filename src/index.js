53282246e12ed49abe60a47cc5eac28b25631435
Huy P <NNewbieDev>
2023-05-08 09:26:49 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import { ContextProvider } from './contexts/ContextProvider';
-import './index.css';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./contexts/ContextProvider";
+import "./index.css";
+import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <ContextProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="react-coffee">
       <App />
     </BrowserRouter>
   </ContextProvider>
   // </React.StrictMode>
 );
-
